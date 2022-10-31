7647920fef4318de8dc7e6451de627024725c2d5
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-10-31 17:49:05 +0300

    Modele Yeni Sınıflar Eklendi

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Process, ProcessDetail, Activity
 
 # Register your models here.
+
+
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('created_date',)
+    # list_editable = ('name',)
+    search_fields = ('name', 'description')
+    list_per_page = 20
+
+
+class ProcessDetailAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'process', 'created_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('created_date',)
+    search_fields = ('name', 'description')
+    list_per_page = 20
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'process_detail', 'created_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('created_date',)
+    search_fields = ('name', 'description')
+    list_per_page = 20
+
+
+admin.site.register(Process, ProcessAdmin)
+admin.site.register(ProcessDetail, ProcessDetailAdmin)
+admin.site.register(Activity,ActivityAdmin)
--- app/models.py
+++ app/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 
 # Create your models here.
-class Service(models.Model):
-    pass
-
-class Customer(models.Model):
-    pass
-
-class Job(models.Model):
-    pass
-
-class Activity(models.Model):
-    pass
 
 class Process(models.Model):
-    pass
+    name=models.CharField(max_length=500,verbose_name='İş Akış Adı')
+    description=models.TextField(verbose_name='İş Akış Açıklaması')
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+
+    def __str__(self):
+        return self.name # + ' - ' + self.created_date.strftime("%d.%m.%Y, %H:%M:%S")
+    
+    class Meta:
+        verbose_name='İş Akış Tanımı'
+        verbose_name_plural="İş Akış Tanımları"
+
+class ProcessDetail(models.Model):
+    process=models.ForeignKey(Process,on_delete=models.CASCADE,verbose_name='İş Akış Tanımı')
+    name=models.CharField(max_length=500,verbose_name='İş Adı')
+    description=models.TextField(verbose_name='İş Açıklama')
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+
+    def __str__(self):
+        return self.name # + ' - ' + self.created_date.strftime("%d.%m.%Y, %H:%M:%S")
+    
+    class Meta:
+        verbose_name="İş Akışı"
+        verbose_name_plural='İş Akışları'
+
+class Activity(models.Model):
+    process_detail=models.ForeignKey(ProcessDetail,on_delete=models.CASCADE,verbose_name='İş Adı')
+    name=models.CharField(max_length=500,verbose_name='Aktivite Adı')
+    description=models.TextField(verbose_name='Aktivite Açıklaması')
+    observation=models.TextField(verbose_name='Gözlem')
+    created_date=models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+
+    def __str__(self):
+        return self.name # + ' - ' + self.created_date.strftime("%d.%m.%Y, %H:%M:%S")
+        
+    class Meta:
+        verbose_name="Aktive"
+        verbose_name_plural='Aktiviteler'
