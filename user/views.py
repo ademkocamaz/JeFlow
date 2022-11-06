13a3946d136d9de603cc630839b3d1211dcfaa3c
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-06 22:17:19 +0300

    Loglar Eklendi

--- user/views.py
+++ user/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
+import logging
 # Create your views here.
+logger=logging.getLogger('db')
 
 def login(request):
     messages.add_message(request,messages.INFO,'demo / demo ile giriş yapabilirsiniz')
@@ -15,9 +17,11 @@ def login(request):
             auth.login(request,user)
             messages.add_message(request,messages.SUCCESS, 'Oturum açıldı.')
             messages.add_message(request,messages.INFO, username + ' Hoşgeldiniz.')
+            logger.info(username + ' kullanıcı adı ile oturum açıldı.')
             return redirect('index')
         else:
             messages.add_message(request,messages.ERROR, 'Kullanıcı adı veya Parola yanlış')
+            logger.error(username + ' kullanıcı adı ile oturum açılırken hata oluştu.')
     
     return render(request, 'user/login.html')
     
@@ -31,15 +35,18 @@ def register(request):
         if password==repassword:
             if User.objects.filter(username=username).exists():
                 messages.add_message(request,messages.WARNING,username+' Kullanıcı adı sistemde kayıtlı.')
+                logger.error(username + ' kullanıcı adı ile kayıt olurken hata oluştu. Sistemde kayıtlı.')
                 return redirect('register')
             else:
                 if User.objects.filter(email=email).exists():
                     messages.add_message(request,messages.WARNING,email+' E-mail adresi sistemde kayıtlı.')
+                    logger.error(email + ' email adresi ile kayıt olurken hata oluştu. Sistemde kayıtlı.')
                     return redirect('register')
                 else:
                     user=User.objects.create_user(username=username,email=email,password=password)
                     user.save()
                     messages.add_message(request,messages.SUCCESS,username+' Hesabınız başarıyla oluşturuldu')
+                    logger.info(username + ' kullanıcı adı ile kayıt başarı ile oluşturuldu.')
                     return redirect('login')
         else:
             messages.add_message(request,messages.ERROR,'Parolalar eşleşmiyor')
@@ -49,6 +56,8 @@ def register(request):
 
 def logout(request):
     if request.method=='POST':
+        username=request.user.username
         auth.logout(request)
         messages.add_message(request,messages.SUCCESS,'Oturumunuz kapatıldı.')
+        logger.info(username + ' oturumu kapatıldı.')
     return redirect('index')
