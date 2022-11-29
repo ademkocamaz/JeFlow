d69468f6d666d7f945d480d8ead3f2baab7cc626
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-29 13:09:59 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
