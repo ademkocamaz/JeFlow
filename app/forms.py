14033593d000f8f5cce5b56795c4c67068d7740c
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-11-29 12:22:10 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -12,17 +12,17 @@ class CategoryForm(forms.ModelForm):
 class ProcessForm(forms.ModelForm):
     class Meta:
         model=Process
-        fields=('category','name','description','state')
+        fields=('category','name','description','state', 'file')
 
 class ActivityForm(forms.ModelForm):
     class Meta:
         model=Activity
-        fields=('process','name','description','observation')
+        fields=('process','name','description','observation', 'file')
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model=Task
-        fields=('process','name','description','assigned_user','state')
+        fields=('process','name','description','assigned_user','state', 'file')
 
 class StateForm(forms.ModelForm):
     class Meta:
